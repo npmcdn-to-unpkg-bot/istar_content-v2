ed8b2e3d07e3d08e2767761ad7323e80d1a66b7f
Mayank prajapati <mayank@istarindia.com>
2016-02-21 23:10:25 +0530

    list of lessons disapproved by creator Signed-off-by: Mayank prajapati <mayank@istarindia.com>

---
+++ src/com/istarindia/cms/controller/LessonDisapprovedController.java
@@ -0,0 +1,80 @@
+package com.istarindia.cms.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.istarindia.apps.StatusTypes;
+import com.istarindia.apps.dao.IstarUser;
+import com.istarindia.apps.dao.IstarUserDAO;
+import com.istarindia.apps.dao.Lesson;
+import com.istarindia.apps.dao.Task;
+import com.istarindia.apps.dao.TaskDAO;
+import com.istarindia.apps.dao.TaskReviewer;
+import com.istarindia.apps.dao.TaskReviewerDAO;
+import com.istarindia.apps.services.LessonService;
+
+/**
+ * Servlet implementation class LessonDisapprovedController
+ */
+@WebServlet("/lesson_disapproved")
+public class LessonDisapprovedController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+    public LessonDisapprovedController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		IstarUser user = (IstarUser)request.getSession().getAttribute("user");
+		ArrayList<ArrayList<String>> list_to_be_displayed = new ArrayList<ArrayList<String>>(); 
+		//id, lessonName, session, module, course,  reviewed_by, mobile preview, desktop preview,view_comment_timeline, edit lesson
+		//these values can be accessed in jsp page to render the data 
+		List<Lesson> lessons= new LessonService().getAllLessonAssignedBy_ContentAdmin(user.getId(), StatusTypes.APPROVED);
+		for(Lesson lesson : lessons)
+		{
+			ArrayList<String> embed_list = new ArrayList<String>();
+			embed_list.add(lesson.getId().toString());
+			embed_list.add(lesson.getTitle());
+			embed_list.add(lesson.getCmsession().getTitle());
+			embed_list.add(lesson.getCmsession().getModule().getModuleName());
+			embed_list.add(lesson.getCmsession().getModule().getCourse().getCourseName());
+			Task task = new TaskDAO().findByItemId(lesson.getId()).get(0);
+			
+			List <TaskReviewer> taskreview = new TaskReviewerDAO().findByProperty("task", task);
+			StringBuffer buff = new StringBuffer();
+			for(TaskReviewer review : taskreview)
+			{
+				buff.append(review.getContentReviewer().getName());
+				buff.append("("+review.getStatus()+")");
+				buff.append(",");
+			}
+			embed_list.add(buff.toString()); //reviewed by
+			embed_list.add(lesson.getId().toString());//view mobile preview
+			embed_list.add(lesson.getId().toString()); // view desktop preview
+			embed_list.add(lesson.getId().toString());// view comment time line
+			embed_list.add(lesson.getId().toString());// edit lesson
+			
+			list_to_be_displayed.add(embed_list);
+		}	
+		request.setAttribute("lessons", list_to_be_displayed);
+		response.sendRedirect(request.getContextPath() + "/content/disapproved_lesson.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
