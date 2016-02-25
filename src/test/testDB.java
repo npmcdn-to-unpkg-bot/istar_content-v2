34d3bb58345c4c9342e4a457fd3c7202bff694a1
Mayank prajapati <mayank@istarindia.com>
2016-02-25 14:41:14 +0530

    Signed-off-by: Mayank prajapati <mayank@istarindia.com>

--- src/test/testDB.java
+++ src/test/testDB.java
@@ -1,7 +1,10 @@
 package test;
 
 import com.istarindia.apps.cmsutils.CMSFolder;
+import com.istarindia.apps.dao.Folder;
 import com.istarindia.apps.services.CMSUtils;
+import com.istarindia.apps.services.FolderService;
+import com.istarindia.apps.services.LessonService;
 
 public class testDB {
 
@@ -11,6 +14,11 @@ public class testDB {
 		StringBuffer sb = new StringBuffer();
 		sb = CMSUtils.getAllFolders();
 		System.out.println("folders>>>"+sb.toString());
+		FolderService d = new FolderService(); 
+		Folder root = d.getRootFolder();
+		StringBuffer sb1 = new StringBuffer();
+		System.out.println(d.getFolderRecursively(root,sb1));
+		System.out.println(d.getSubFolders(root).size());
 	}
 
 }
