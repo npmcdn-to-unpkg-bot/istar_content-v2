fa714dfedf7dbe79bc116edd0ce7191003c7b2a9
Suryateja M V <surya@istarindia.com>
2016-03-01 10:54:17 +0530

    All text validation
    Signed-off-by: Suryateja M V <surya@istarindia.com>

--- WebContent/assets/js/plugins/validation.js
+++ WebContent/assets/js/plugins/validation.js
@@ -3,7 +3,7 @@ var Validation = function () {
     return {
         
         //Validation
-        initValidation: function () {
+    	createMediaTaskValidation: function () {
 	        $("#sky-form1").validate({                   
 	            // Rules for form validation
 	            rules:
@@ -15,61 +15,6 @@ var Validation = function () {
 	            	description:
 	                {
 	                    required: true
-	                },
-	                email:
-	                {
-	                    required: true,
-	                    email: true
-	                },
-	                url:
-	                {
-	                    required: true,
-	                    url: true
-	                },
-	                date:
-	                {
-	                    required: true,
-	                    date: true
-	                },
-	                min:
-	                {
-	                    required: true,
-	                    minlength: 5
-	                },
-	                max:
-	                {
-	                    required: true,
-	                    maxlength: 5
-	                },
-	                range:
-	                {
-	                    required: true,
-	                    rangelength: [5, 10]
-	                },
-	                digits:
-	                {
-	                    required: true,
-	                    digits: true
-	                },
-	                number:
-	                {
-	                    required: true,
-	                    number: true
-	                },
-	                minVal:
-	                {
-	                    required: true,
-	                    min: 5
-	                },
-	                maxVal:
-	                {
-	                    required: true,
-	                    max: 100
-	                },
-	                rangeVal:
-	                {
-	                    required: true,
-	                    range: [5, 100]
 	                }
 	            },
 	                                
@@ -83,50 +28,6 @@ var Validation = function () {
 	            	description:
 	                {
 	                    required: 'Please enter the task description(Tags and target folders)'
-	                },
-	                email:
-	                {
-	                    required: 'Please enter your email address'
-	                },
-	                url:
-	                {
-	                    required: 'Please enter your URL'
-	                },
-	                date:
-	                {
-	                    required: 'Please enter some date'
-	                },
-	                min:
-	                {
-	                    required: 'Please enter some text'
-	                },
-	                max:
-	                {
-	                    required: 'Please enter some text'
-	                },
-	                range:
-	                {
-	                    required: 'Please enter some text'
-	                },
-	                digits:
-	                {
-	                    required: 'Please enter some digits'
-	                },
-	                number:
-	                {
-	                    required: 'Please enter some number'
-	                },
-	                minVal:
-	                {
-	                    required: 'Please enter some value'
-	                },
-	                maxVal:
-	                {
-	                    required: 'Please enter some value'
-	                },
-	                rangeVal:
-	                {
-	                    required: 'Please enter some value'
 	                }
 	            },                  
 	            
@@ -136,7 +37,80 @@ var Validation = function () {
 	                error.insertAfter(element.parent());
 	            }
 	        });
-        }
+        },
 
+        createFolderValidation: function () {
+	        $("#sky-form1").validate({                   
+	            // Rules for form validation
+	            rules:
+	            {
+	            	folder_name:
+	            	{
+	            		required: true,
+	            		rangelength: [5, 50]
+	            	}
+	            },
+	                                
+	            // Messages for form validation
+	            messages:
+	            {
+	            	folder_name:
+	            	{
+	            		required: 'Pleaes enter a valid folder name'
+	            	}
+	            },                  
+	            
+	            // Do not change code below
+	            errorPlacement: function(error, element)
+	            {
+	                error.insertAfter(element.parent());
+	            }
+	        });
+        },
+    lessonValidation: function () {
+        $("#sky-form4").validate({                   
+            // Rules for form validation
+            rules:
+            {
+            	title:
+            	{
+            		required: true
+            	},
+            	duration:
+                {
+                    required: true
+                },
+            	tags:
+            	{
+            		required: true
+            	}
+            },
+                                
+            // Messages for form validation
+            messages:
+            {
+            	title:
+            	{
+            		required: 'Pleaes enter valid lesson title'
+            	},
+            	duration:
+                {
+                    required: 'Please enter valid duration',
+                    min: 1
+                },
+            	tags:
+            	{
+            		required: 'Please enter at least one tag',
+            		min: 1
+            	}
+            },                  
+            
+            // Do not change code below
+            errorPlacement: function(error, element)
+            {
+                error.insertAfter(element.parent());
+            }
+        });
+    }
     };
 }();
