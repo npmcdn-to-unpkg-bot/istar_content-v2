49c1f52e321bf2dd0e9c567852af9f3974d5e47a
kunal-mastiska <kunal.chakravertti@mastiska.com>
2016-03-30 13:20:57 +0530

    Committed By Kunal

--- src/com/istarindia/cms/controller/TestController.java
+++ src/com/istarindia/cms/controller/TestController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.istarindia.cms.controller;  
+package com.istarindia.cms.controller;   
 
 import com.istarindia.apps.dao.Assessment;
 import com.istarindia.apps.dao.LearningObjective;
