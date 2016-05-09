38ce2d724ff7b582816eb513220f835ae559c188
mayank <mayank@istarindia.com>
2016-05-09 17:43:40 +0530

    Signed-off-by: mayank <mayank@istarindia.com>

--- src/com/istarindia/apps/services/CMSRegistry.java
+++ src/com/istarindia/apps/services/CMSRegistry.java
@@ -96,7 +96,7 @@ public class CMSRegistry {
 		slideTemplates.add("ONLY_IMAGE");
 		slideTemplates.add("ONLY_PARAGRAPH_IMAGE");
 		slideTemplates.add("ONLY_PARAGRAPH");
-		slideTemplates.add("ONLY_TITLE_IMAGE_PARAGRPH");
+		slideTemplates.add("ONLY_TITLE_IMAGE_PARAGRAPH");
 		slideTemplates.add("ONLY_TITLE_IMAGE");
 		slideTemplates.add("ONLY_TITLE_LIST");
 		slideTemplates.add("ONLY_TITLE_PARAGRAPH");
