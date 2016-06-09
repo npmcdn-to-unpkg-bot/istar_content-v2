421bfaa27aaf5c47dce0d44af6c774d0c3094ada
Administrator <Administrator@172.31.16.114>
2016-06-09 15:11:26 +0000

    Signed-off-by: Administrator <Administrator@172.31.16.114>

--- src/com/istarindia/apps/cmsutils/reports/column/HandoutColumnHandler.java
+++ src/com/istarindia/apps/cmsutils/reports/column/HandoutColumnHandler.java
@@ -7,7 +7,7 @@ public class HandoutColumnHandler extends ColumnHandler {
 	@Override
 	public StringBuffer getHTML(String status, IstarUser user, String taskType, int taskID, int reportID) {
 		
-		return new StringBuffer("<a href='/content/content_creator/edit_handout.jsp?handout_id=" + taskID + "'> Edit</a>  ");
+		return new StringBuffer("<a href='/content/content_creator/edit_handout.jsp?lesson_id=" + taskID + "'> Edit</a>  ");
 		//return null;
 	}
 
